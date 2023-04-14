534ea574f17ede669facf41fa26b1413c9d5dc4f
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-14 20:34:01 +0900

    feat: 環境変数configファイル

---
+++ M5Unified_StackChan_ChatGPT/src/config.hpp
@@ -0,0 +1,12 @@
+// config.hpp
+
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+extern const char *OPENAI_API_KEY;
+extern const char *WIFI_SSID;
+extern const char *WIFI_PASSWORD;
+extern const char *API_URL;
+extern const char *VOICE_API_KEY;
+
+#endif
